2e108578229b6c0da5c05d77f74b61af45702907
osirisofgit <github.com.j95fw@passmail.net>
2025-08-30 11:37:19 -0500

    debug and license

--- HitChanceToDieRoll/Mods/HitChanceToDieRoll/ScriptExtender/Lua/BootstrapClient.lua
+++ HitChanceToDieRoll/Mods/HitChanceToDieRoll/ScriptExtender/Lua/BootstrapClient.lua
@@ -1,5 +1,8 @@
 Ext.Require("Utilities/Common/_Index.lua")
 Ext.Require("Utilities/Networking/Channels.lua")
-Ext.Require("Utilities/Advanced/_ECSPrinter.lua")
+Ext.Require("Utilities/Client/IMGUI/_Index.lua")
 
 Logger:ClearLogFile()
+
+
+
