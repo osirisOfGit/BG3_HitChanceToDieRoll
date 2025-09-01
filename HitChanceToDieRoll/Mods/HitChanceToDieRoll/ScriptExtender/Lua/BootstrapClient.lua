684b42b99aa01cba611d2fd6e99e9ad976c3d1b5
osirisofgit <github.com.j95fw@passmail.net>
2025-09-01 11:21:03 -0500

    fixes

--- HitChanceToDieRoll/Mods/HitChanceToDieRoll/ScriptExtender/Lua/BootstrapClient.lua
+++ HitChanceToDieRoll/Mods/HitChanceToDieRoll/ScriptExtender/Lua/BootstrapClient.lua
@@ -31,10 +31,12 @@ local function recursiveFindForHitChance(parent)
 end
 
 local showPercentage = false
-
 local haveLogged = false
+local alreadyChanged = false
+
 local tickSub
 Channels.FireAway:SetHandler(function(_, _)
+	local isSavingThrow
 	tickSub = Ext.Events.Tick:Subscribe(function(e)
 		local parentNode = Ext.UI.GetRoot():Child(1):Child(1)
 
@@ -53,11 +55,13 @@ Channels.FireAway:SetHandler(function(_, _)
 		if hitNode then
 			local dataContext = hitNode:GetProperty("DataContext"):GetAllProperties()
 
-			if dataContext.ActiveTask:GetProperty("RootCastSpell") then
+			if dataContext.ActiveTask.PreviewType == "Spell" then
 				local hitChanceNode = dataContext.HitChanceDesc:GetAllProperties()
 
 				if hitChanceNode.ShowDescription then
-					if (hitNode:GetProperty("Name") == "hitChanceText") then
+					if not alreadyChanged then
+						alreadyChanged = true
+
 						if hitChanceNode.TotalHitChance == 100 then
 							if showPercentage then
 								hitNode:SetProperty("Text", ("%s%%"):format(tostring(100)))
@@ -65,23 +69,33 @@ Channels.FireAway:SetHandler(function(_, _)
 								hitNode:SetProperty("Text", ("DC: %s"):format(tostring(0)))
 							end
 						else
+							hitNode.Foreground:SetProperty("Color", { 1, 0, 0, 1 })
 							if showPercentage then
 								hitNode:SetProperty("Text", ("%s%%"):format(hitChanceNode.TotalHitChance))
 							else
-								---@type SpellData
-								local spellData = Ext.Stats.Get(dataContext.ActiveTask.RootCastSpell.PrototypeID)
-								local hitChance = math.ceil(hitChanceNode.TotalHitChance / 5) * 5
-								local dc = hitChance > 0 and math.floor(math.max(2, (20 - (20 * (hitChance / 100))) + 1)) or 20
-								if spellData.TooltipAttackSave and Ext.Enums.AbilityId[spellData.TooltipAttackSave] then
-									dc = 20 - (dc - 1)
+								if isSavingThrow == nil and dataContext.ActiveTask.RootCastSpell then
+									---@type SpellData
+									local spellData = Ext.Stats.Get(dataContext.ActiveTask.RootCastSpell.PrototypeID)
+									isSavingThrow = not spellData.SpellRoll.Default:find("Attack%(")
+								end
+
+								local dc
+								if isSavingThrow then
+									dc = math.ceil(20 * (hitChanceNode.TotalHitChance / 100)) + 1
+								else
+									local hitChance = math.ceil(hitChanceNode.TotalHitChance / 5) * 5
+									dc = hitChance > 0 and math.floor(math.max(2, (20 - (20 * (hitChance / 100))) + 1)) or 20
 								end
 								hitNode:SetProperty("Text", ("DC: %s"):format(dc))
 							end
 						end
 					end
+				else
+					alreadyChanged = false
 				end
 			elseif tickSub then
 				Ext.Events.Tick:Unsubscribe(tickSub)
+				alreadyChanged = false
 				tickSub = nil
 			end
 		else
@@ -104,6 +118,7 @@ Ext.Events.KeyInput:Subscribe(
 		if e.Event == "KeyDown" and e.Repeat == false then
 			local lshift, lalt = Ext.Enums.SDLKeyModifier.LShift, Ext.Enums.SDLKeyModifier.LAlt
 			if e.Key == "P" and e.Modifiers & lshift == lshift and e.Modifiers & lalt == lalt then
+				alreadyChanged = false
 				showPercentage = not showPercentage
 			end
 		end
