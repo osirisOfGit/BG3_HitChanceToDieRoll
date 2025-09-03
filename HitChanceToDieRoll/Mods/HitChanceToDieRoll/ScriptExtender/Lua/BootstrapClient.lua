000d064a49882a249a085d47dd0372f4232c6a74
osirisofgit <github.com.j95fw@passmail.net>
2025-09-03 09:05:08 -0500

    fix

--- HitChanceToDieRoll/Mods/HitChanceToDieRoll/ScriptExtender/Lua/BootstrapClient.lua
+++ HitChanceToDieRoll/Mods/HitChanceToDieRoll/ScriptExtender/Lua/BootstrapClient.lua
@@ -38,71 +38,80 @@ local tickSub
 Channels.FireAway:SetHandler(function(_, _)
 	local isSavingThrow
 	tickSub = Ext.Events.Tick:Subscribe(function(e)
-		local parentNode = Ext.UI.GetRoot():Child(1):Child(1)
+		local success, error = xpcall(function(...)
+			local parentNode = Ext.UI.GetRoot():Child(1):Child(1)
 
-		-- Noesis nodes have a lifetime that expires every tick, and mods can overwrite the Cursor.xaml
-		-- so we can't hardcode a path
-		local hitNode
-		if next(childPath) then
-			for _, visualChildIndex in ipairs(childPath) do
-				parentNode = parentNode:VisualChild(visualChildIndex)
+			-- Noesis nodes have a lifetime that expires every tick, and mods can overwrite the Cursor.xaml
+			-- so we can't hardcode a path
+			local hitNode
+			if next(childPath) then
+				for _, visualChildIndex in ipairs(childPath) do
+					parentNode = parentNode:VisualChild(visualChildIndex)
+				end
+				hitNode = parentNode
+			else
+				hitNode = recursiveFindForHitChance(parentNode)
 			end
-			hitNode = parentNode
-		else
-			hitNode = recursiveFindForHitChance(parentNode)
-		end
 
-		if hitNode then
-			local dataContext = hitNode:GetProperty("DataContext"):GetAllProperties()
+			if hitNode then
+				local dataContext = hitNode:GetProperty("DataContext"):GetAllProperties()
 
-			if dataContext.ActiveTask.PreviewType == "Spell" then
-				local hitChanceNode = dataContext.HitChanceDesc:GetAllProperties()
+				if dataContext.ActiveTask.PreviewType == "Spell" then
+					local hitChanceNode = dataContext.HitChanceDesc:GetAllProperties()
 
-				if hitChanceNode.ShowDescription then
-					if not alreadyChanged then
-						alreadyChanged = true
+					if hitChanceNode.ShowDescription then
+						if not alreadyChanged then
+							alreadyChanged = true
 
-						if hitChanceNode.TotalHitChance == 100 then
-							if showPercentage then
-								hitNode:SetProperty("Text", ("%s%%"):format(tostring(100)))
-							else
-								hitNode:SetProperty("Text", ("DC: %s"):format(tostring(0)))
-							end
-						else
-							hitNode.Foreground:SetProperty("Color", { 1, 0, 0, 1 })
-							if showPercentage then
-								hitNode:SetProperty("Text", ("%s%%"):format(hitChanceNode.TotalHitChance))
-							else
-								if isSavingThrow == nil and dataContext.ActiveTask.RootCastSpell then
-									---@type SpellData
-									local spellData = Ext.Stats.Get(dataContext.ActiveTask.RootCastSpell.PrototypeID)
-									isSavingThrow = not spellData.SpellRoll.Default:find("Attack%(")
-								end
-
-								local dc
-								if isSavingThrow then
-									dc = math.ceil(20 * (hitChanceNode.TotalHitChance / 100)) + 1
+							if hitChanceNode.TotalHitChance == 100 then
+								if showPercentage then
+									hitNode:SetProperty("Text", ("%s%%"):format(tostring(100)))
 								else
-									local hitChance = math.ceil(hitChanceNode.TotalHitChance / 5) * 5
-									dc = hitChance > 0 and math.floor(math.max(2, (20 - (20 * (hitChance / 100))) + 1)) or 20
+									hitNode:SetProperty("Text", ("DC: %s"):format(tostring(0)))
 								end
-								hitNode:SetProperty("Text", ("DC: %s"):format(dc))
+							else
+								hitNode.Foreground:SetProperty("Color", { 1, 0, 0, 1 })
+								if showPercentage then
+									hitNode:SetProperty("Text", ("%s%%"):format(hitChanceNode.TotalHitChance))
+								else
+									if isSavingThrow == nil and dataContext.ActiveTask.RootCastSpell then
+										---@type SpellData
+										local spellData = Ext.Stats.Get(dataContext.ActiveTask.RootCastSpell.PrototypeID)
+										isSavingThrow = not spellData.SpellRoll.Default:find("Attack%(")
+									end
+
+									local dc
+									if isSavingThrow then
+										dc = math.ceil(20 * (hitChanceNode.TotalHitChance / 100)) + 1
+									else
+										local hitChance = math.ceil(hitChanceNode.TotalHitChance / 5) * 5
+										dc = hitChance > 0 and math.floor(math.max(2, (20 - (20 * (hitChance / 100))) + 1)) or 20
+									end
+									hitNode:SetProperty("Text", ("DC: %s"):format(dc))
+								end
 							end
 						end
+					else
+						alreadyChanged = false
 					end
-				else
+				elseif tickSub then
+					Ext.Events.Tick:Unsubscribe(tickSub)
 					alreadyChanged = false
+					tickSub = nil
 				end
-			elseif tickSub then
-				Ext.Events.Tick:Unsubscribe(tickSub)
-				alreadyChanged = false
-				tickSub = nil
+			else
+				if not haveLogged then
+					haveLogged = true
+					Logger:BasicError("Couldn't locate the hitChanceText node? Report to Osirisofinternet with all your mods that affect the UI")
+				end
+				if tickSub then
+					Ext.Events.Tick:Unsubscribe(tickSub)
+					tickSub = nil
+				end
 			end
-		else
-			if not haveLogged then
-				haveLogged = true
-				Logger:BasicError("Couldn't locate the hitChanceText node? Report to Osirisofinternet with all your mods that affect the UI")
-			end
+		end, debug.traceback)
+		if not success then
+			Logger:BasicError("Error occurred during tick processing, report scenario to Osirisofinternet: %s", error)
 			if tickSub then
 				Ext.Events.Tick:Unsubscribe(tickSub)
 				tickSub = nil
